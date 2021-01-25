9a86e87f856d242562cafa80cc7e4d2c88efe152
dtrihinas <trihinas@cs.ucy.ac.cy>
2021-01-25 15:24:55 +0200

    timer metric

--- Catascopia/Metrics.py
+++ Catascopia/Metrics.py
@@ -1,15 +1,18 @@
 import time
-
+import threading
 
 class Metric(object):
 
-    def __init__(self, name, units, desc):
+    def __init__(self, name, units, desc, minVal = None, maxVal = None, higherIsBetter=True):
         self.name = name
         self.units = units
         self.desc = desc
+        self.higherIsBetter = higherIsBetter
+        self.minVal = minVal
+        self.maxVal = maxVal
+        self.group = None
         self.val = None
         self.timestamp = None
-        self.group = None
 
     def get_name(self):
         return self.name
@@ -47,28 +50,6 @@ class Metric(object):
     def set_group(self, group):
         self.group = group
 
-    def to_dict(self):
-        d = dict()
-        d['name'] = self.name
-        d['units'] = self.units
-        d['desc'] = self.desc
-        d['timestamp'] = self.timestamp
-        d['val'] = self.val
-        d['group'] = self.group
-        return d
-
-    def __str__(self):
-        return str(self.to_dict())
-
-
-class SimpleMetric(Metric):
-
-    def __init__(self, name, units, desc, minVal = float('-inf'), maxVal = float('inf'), higherIsBetter=True):
-        super(SimpleMetric, self).__init__(name, units, desc)
-        self.higherIsBetter = higherIsBetter
-        self.minVal = minVal
-        self.maxVal = maxVal
-
     def get_higherisbetter(self):
         return self.higherIsBetter
 
@@ -87,16 +68,32 @@ class SimpleMetric(Metric):
     def set_maxval(self, maxVal):
         self.maxVal = maxVal
 
+    def to_dict(self):
+        d = dict()
+        d['name'] = self.name
+        d['units'] = self.units
+        d['desc'] = self.desc
+        d['timestamp'] = self.timestamp
+        d['val'] = self.val
+        d['higherIsBetter'] = self.higherIsBetter
+        d['minVal'] = self.minVal
+        d['maxVal'] = self.maxVal
+        d['group'] = self.group
+        return d
+
+    def __str__(self):
+        return str(self.to_dict())
+
+
+class SimpleMetric(Metric):
+
+    def __init__(self, name, units, desc, minVal = float('-inf'), maxVal = float('inf'), higherIsBetter=True):
+        super(SimpleMetric, self).__init__(name, units, desc, minVal, maxVal, higherIsBetter)
+
     def set_val(self, val):
         self.timestamp = int(time.time())
         self.val = val
 
-    def __str__(self):
-        m = super().to_dict()
-        m['higherIsBetter'] = self.higherIsBetter
-        m['minVal'] = self.minVal
-        m['maxVal'] = self.maxVal
-        return str(m)
 
 class CounterMetric(SimpleMetric):
     def __init__(self, name, units, desc, minVal = 0, maxVal = float('inf'), higherIsBetter=True, step=1, reset=True):
@@ -106,9 +103,9 @@ class CounterMetric(SimpleMetric):
         self.reset = reset
 
     def inc(self):
-        self._inc(self.step)
+        self.inc_with_step(self.step)
 
-    def _inc(self, step):
+    def inc_with_step(self, step):
         if isinstance(step, int):
             self.counter += step
             if self.counter > self.maxVal:
@@ -141,9 +138,68 @@ class DiffMetric(SimpleMetric):
             self.diff = 0
         self.set_val(self.diff * 100)
 
-# TODO implement TimerMetric class
-class TimerMetric(Metric):
-    pass
+
+#TODO add some exceptions
+class TimerMetric(SimpleMetric):
+
+    IDLE, STARTED, PAUSED, FINISHED = range(4)
+
+    # maxVal is the max time the timer will wait each time before returning a value, default 24hours
+    # ensures timer not blocking forever
+    def __init__(self, name, units='s', desc='a timer metric', minVal=0, maxVal=86400, higherIsBetter=False):
+        super(TimerMetric, self).__init__(name, units, desc, minVal, maxVal, higherIsBetter)
+        self.tstart = 0;
+        self.tval = 0
+        self.timer = None
+        self.timer_status = TimerMetric.IDLE
+
+    def timer_start(self):
+        if self.timer_status == TimerMetric.IDLE or self.timer_status == TimerMetric.PAUSED:
+            self.timer = threading.Timer(0.0, self._waiting_clock_expire, [self.maxVal])
+            self.tstart = time.time()
+            self.timer.start()
+            self.timer_status = TimerMetric.STARTED
+            return True
+        return False
+
+    def timer_pause(self):
+        if self.timer_status == TimerMetric.STARTED:
+            self.tval += time.time() - self.tstart
+            self.timer.cancel()
+            self.timer_status = TimerMetric.PAUSED
+            self.set_val(self.tval)
+            return True
+        return False
+
+    def timer_end(self):
+        if self.timer_status == TimerMetric.STARTED:
+            self.tval += time.time() - self.tstart
+        if self.timer_status == TimerMetric.STARTED or self.timer_status == TimerMetric.PAUSED:
+            self.set_val(self.tval)
+            if self.timer:
+                self.timer.cancel()
+            self.timer_status = TimerMetric.FINISHED
+            print(self.val)
+            return True
+        return False
+
+    def timer_reset(self):
+        self.timer_status = TimerMetric.IDLE
+        self.tval = 0
+        self.tstart = 0
+        if self.timer:
+            self.timer.cancel()
+        self.timer = None
+        return True
+
+    def timer_reset_and_start(self):
+        r = self.timer_reset()
+        s = self.timer_start()
+        return r and s
+
+    def _waiting_clock_expire(self, max_wait_time):
+        time.sleep(max_wait_time)
+        self.timer_end()
 
 
 class CatascopiaMetricValueException(Exception):
